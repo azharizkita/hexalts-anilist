1a5984ded569a4ca60e0eeac016fadee04627836
azharizkita <azharizkita@icloud.com>
2023-08-07 13:07:03 +0700

    feat: remove button outlines on focus

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export const theme = extendTheme();
+export const theme = extendTheme({
+  components: { Button: { baseStyle: { _focus: { boxShadow: "none" } } } },
+});
